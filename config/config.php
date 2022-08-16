5c6db42e3bfd509377f63a6bd87bd4454a044d62
mpgithubcode <github@jobs.slmail.me>
2022-08-16 01:51:18 +0000

    Updated to 24.0.4

---
+++ config/config.php
@@ -0,0 +1,33 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3zxprtbkng',
+  'passwordsalt' => 'iWueUDj5NDfmZl7kNvwYGeKZVayam2',
+  'secret' => 'w6TmRD+k31UodmrZrVNwZREKl6u3xHUBzDbBoGxuu+/pQFQd',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.1.168',
+  ),
+  'datadirectory' => '/var/www/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '24.0.4.1',
+  'overwrite.cli.url' => 'http://192.168.1.168',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'bballmp95',
+  'installed' => true,
+  'updater.secret' => '$2y$10$wQDSm36OHt1sPsa5h.zH..Mb0vUSOyqooAlfCjtbwZFlIWpYsT/0q',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'default_phone_region' => 'US',
+  'maintenance' => false,
+  'theme' => '',
+  'loglevel' => 2,
+  'app_install_overwrite' => 
+  array (
+    0 => 'documentserver_community',
+    1 => 'files_texteditor',
+  ),
+);
